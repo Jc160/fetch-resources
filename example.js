3132ac6da1e7af59190b9fe1af07f0b69be2e5bd
Julio Ramirez <jc16018@gmail.com>
2017-11-24 12:19:41 -0400

    Add initial fetch resources implementation

---
+++ example.js
@@ -0,0 +1,137 @@
+'use strict';
+
+
+const FetchResources = require('./index.js');
+
+
+////////////////////////////////////////////////////////////
+
+
+//Fetch Resource initial config
+const objConfig = {
+  host: 'https://jsonplaceholder.typicode.com'
+};
+
+
+//Fetch Resource Instance
+const objFetchResources = FetchResources(objConfig);
+
+
+//Post Source
+const objPostSource = objFetchResources.source('posts', {
+  getById: {
+    method: 'GET',
+    endpoint: 'posts/:id',
+    params: ['id']
+  },
+  getPostComments: {
+    method: 'GET',
+    endpoint: 'posts/:id/comments',
+    params: ['id']
+  }
+});
+
+
+////////////////////////////////////////////////////////////
+
+
+// GET https://jsonplaceholder.typicode.com/posts HTTP/1.1
+objPostSource.get().then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
+
+
+// GET https://jsonplaceholder.typicode.com/posts?userId=1 HTTP/1.1
+objPostSource.get({ userId: 1 }).then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
+
+
+// POST https://jsonplaceholder.typicode.com/posts HTTP/1.1
+const objCreatePayload = {
+  title: 'foo',
+  body: 'bar',
+  userId: 1
+};
+
+objPostSource.post(objCreatePayload).then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
+
+
+// PUT https://jsonplaceholder.typicode.com/posts/1 HTTP/1.1
+const objUpdatePayload = {
+  id: 1, // This will be added to the URL
+  title: 'foo2',
+  body: 'bar',
+  userId: 1
+};
+
+objPostSource.update(objUpdatePayload).then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
+
+
+// DELETE https://jsonplaceholder.typicode.com/posts/1 HTTP/1.1
+const objDeletePayload = {
+  id: 1, // This will be added to the URL
+};
+
+objPostSource.remove(objDeletePayload).then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
+
+
+// GET https://jsonplaceholder.typicode.com/posts/1 HTTP/1.1
+const objGetSinglePayload = {
+  id: 1, // This will be added to the URL
+};
+
+objPostSource.getById(objGetSinglePayload).then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
+
+
+// GET https://jsonplaceholder.typicode.com/posts/1/comments HTTP/1.1
+const objGetCommentsPayload = {
+  id: 1, // This will be added to the URL
+};
+
+objPostSource.getPostComments(objGetCommentsPayload).then((results) => {
+  // success
+  console.log(results);
+})
+.catch((error) => {
+  // error
+  console.log(error);
+});
---
+++ index.js
@@ -0,0 +1,265 @@
+'use strict';
+
+
+const fetch = require('node-fetch');
+const Lodash = require('lodash');
+const QueryString = require('query-string');
+
+
+////////////////////////////////////////////////////////////
+
+
+/**
+ * Default headers
+ * @type {Object}
+ */
+const HEADERS = {
+ 'Accept': 'application/json',
+ 'Content-Type': 'application/json',
+}
+
+
+/**
+ * Axios Data field validation.
+ * @type {Array}
+ */
+const DATA_VALID_METHODS = ['PUT', 'POST', 'PATCH'];
+
+
+/**
+ * Requester Creator
+ * @param {Object} objConfig Init config
+ */
+function Requester(objConfig={}) {
+  const INTERNAL_HEADERS = Object.assign({}, HEADERS, objConfig.headers);
+
+
+  const fnAfterFetchFn = objConfig.afterFetch || afterFetch;
+  const fnBeforeFetchFn = objConfig.beforeFetchFn || beforeFetch;
+  const fnOnError = objConfig.onError || onError;
+  const fnOnRequestError = objConfig.onRequestError || onRequestError;
+
+
+  function mergeWithBaseUrl(strEndpointUrl) {
+    return objConfig.host ? `${objConfig.host}/${strEndpointUrl}` : strEndpointUrl;
+  }
+
+
+  /**
+   * Axios Request
+   * @param  {Object} objRequestConfig      Fetch Config
+   * @return {Fuction}            Promise
+   */
+  function request(objRequestConfig, objCustomEndpoint) {
+    return fnBeforeFetchFn(objRequestConfig, objCustomEndpoint).then((listResults) => {
+
+      let [objRequestConfig, objCustomEndpoint] = listResults;
+      const strRequestUrl = mergeWithBaseUrl(objRequestConfig.url);
+      const objInternalRequest = Lodash.omit(objRequestConfig, ['url']);
+
+      return fetch(strRequestUrl, objInternalRequest)
+        .then((varResponse) => {
+          return fnAfterFetchFn(varResponse, objRequestConfig, objCustomEndpoint);
+        })
+        .catch(fnOnRequestError);
+
+    }).catch(fnOnError);
+  }
+
+
+  /**
+   * This function handles the request error
+   * @param  {Object} objError Error object
+   * @return {Promise}
+   */
+  function onRequestError(objError) {
+    if (objError.then) {
+      return objError.then((objNestedError) => {
+        return Promise.reject(objNestedError);
+      });
+    }
+    return Promise.reject(objError);
+  }
+
+
+  /**
+   * This function handles the beforeFetch.
+   */
+  function beforeFetch(objRequestConfig, objCustomEndpoint) {
+    return Promise.resolve([objRequestConfig, objCustomEndpoint]);
+  }
+
+
+  /**
+   * Handle success response request
+   * @return {Object}
+   */
+  function afterFetch(varResponse, objRequestConfig, objCustomEndpoint) {
+    if (objCustomEndpoint.isRaw) {
+      return varResponse;
+    }
+
+    if (varResponse.status === 413) {
+      return varResponse.text();
+    }
+
+    if (varResponse.status >= 200 && varResponse.status < 300) {
+      if (varResponse.status === 204) {
+        return null;
+      }
+      return varResponse.json();
+    } else {
+      return Promise.reject(varResponse.json());
+    }
+  }
+
+
+  /**
+   * Handle error request
+   * @param  {Object} objResponse
+   * @return {Object}
+   */
+  function onError(objResponse) {
+    return objResponse;
+  }
+
+
+  /**
+   * Transform endpoint replacing key params
+   * @param  {Object} objCustomApi Custom api data
+   * @param  {Object} objData      Request data
+   * @return {Array}
+   */
+  function getCustomEndpoint(objCustomApi, objData) {
+    let strEndpoint = objCustomApi.endpoint;
+    objCustomApi.params && objCustomApi.params.forEach((strParam) => {
+      strEndpoint = strEndpoint.replace(`:${strParam}`, objData[strParam]);
+    });
+
+
+    return strEndpoint;
+  }
+
+
+  /**
+   * This function creates the custom api object. (Axios Config Object)
+   * @param  {Object} objCustomEndpoint Custom API Config
+   * @param  {Object} objSourceHeaders  Initial Headers
+   * @return {Function}                   Request function
+   */
+  function createCustomApi(objCustomEndpoint, objSourceHeaders) {
+    const objInternalHeaders = Object.assign(
+      {},
+      objSourceHeaders,
+      objCustomEndpoint.headers
+    );
+
+
+    return (objData, objNewConfig={}) => {
+
+      const hasBody = Lodash.includes(DATA_VALID_METHODS, objCustomEndpoint.method.toUpperCase());
+
+      const hasQueryParams = !Lodash.isEmpty(objData)
+        && !hasBody;
+
+
+      const objCleanData = Lodash.omit(objData, objCustomEndpoint.params);
+
+      const strUrl = hasQueryParams
+      ? `${getCustomEndpoint(objCustomEndpoint, objData)}?${QueryString.stringify(objCleanData)}`
+      : getCustomEndpoint(objCustomEndpoint, objData);
+
+      let initialConfig = {
+        method: objCustomEndpoint.method,
+        url: strUrl,
+        headers: (
+          {},
+          objInternalHeaders,
+          objNewConfig.addHeaders
+        )
+      };
+
+      if (hasBody) {
+        initialConfig['body'] = objCleanData;
+      }
+
+      let objConfig = Object.assign(
+        initialConfig,
+        objCustomEndpoint.options,
+        Lodash.omit(objNewConfig, ['addHeaders'])
+      );
+
+      return request(objConfig, objCustomEndpoint);
+    }
+  }
+
+
+  /**
+   * This function transform the customs config objects to custom functions.
+   * @param  {Object} objCustomEndpoint Customs endpoints config
+   * @param  {Object} objSourceHeaders  Initial Headers
+   * @return {Object}                   Custom endpoints functions
+   */
+  function getCustomEndpoints(objCustomEndpoint, objSourceHeaders) {
+    return Object.keys(objCustomEndpoint).reduce((objCustomEndpoints, strKey) => {
+      objCustomEndpoints[strKey] = createCustomApi(objCustomEndpoint[strKey], objSourceHeaders);
+      return objCustomEndpoints;
+    }, {});
+  }
+
+
+  /**
+   * This function generates the source object with requests functions.
+   * @param  {[type]} strEndpoint        Source Endpoint
+   * @param  {Object} objCustomEndpoints Custom Endpoints object
+   * @param  {[type]} objSourceHeaders   Custom source headers
+   * @return {[type]}                    [description]
+   */
+  function source(strEndpoint, objCustomEndpoints={}, objSourceHeaders) {
+    const objInternalHeaders = Object.assign({}, INTERNAL_HEADERS, objSourceHeaders);
+
+    const objEndpointWithId = {
+      endpoint: `${strEndpoint}/:id`,
+      params: ['id']
+    };
+
+
+    const fnGet = createCustomApi({ method: 'GET', endpoint: strEndpoint }, objInternalHeaders);
+    const fnPut = createCustomApi(Object.assign({ method: 'PUT' }, objEndpointWithId), objInternalHeaders);
+    const fnDelete = createCustomApi(Object.assign({ method: 'DELETE' }, objEndpointWithId), objInternalHeaders);
+
+
+    const fnPost = createCustomApi(
+      {
+        method: 'POST',
+        endpoint: strEndpoint,
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      },
+      objInternalHeaders
+    );
+
+
+    return Object.assign(
+      {
+        get: fnGet,
+        post: fnPost,
+        update: fnPut,
+        remove: fnDelete
+      },
+      getCustomEndpoints(objCustomEndpoints, objInternalHeaders)
+    )
+  }
+
+
+  return {
+    source,
+    request,
+    fetch
+  }
+}
+
+
+////////////////////////////////////////////////////////////
+
+
+module.exports = Requester;
